8c82c658a963dcacc6907f58ef4750ff2e91ecfe
charagarlnad <27380203+charagarlnad@users.noreply.github.com>
2019-10-24 02:23:25 -0400

    Update arch_install.sh

--- arch_install.sh
+++ arch_install.sh
@@ -41,6 +41,8 @@ mount /dev/sda2 /mnt
 mkdir -p /mnt/boot
 mount /dev/sda1 /mnt/boot
 
+root_uuid=$(blkid -o value -s UUID /dev/sda2)
+
 echo 'Installing packages...'
 pacstrap /mnt base linux linux-firmware fakeroot make gcc binutils patch dialog nano efibootmgr git sudo wpa_supplicant networkmanager
 
@@ -94,7 +96,7 @@ echo \
 > /etc/hosts
 
 echo 'Installing EFISTUB...'
-efibootmgr --disk /dev/sda --part 1 --create --label 'Arch' --loader /vmlinuz-linux --unicode 'root=/dev/sda rw mitigations=off initrd=\initramfs-linux.img'
+efibootmgr --disk /dev/sda --part 1 --create --label 'Arch' --loader /vmlinuz-linux --unicode "root=${root_uuid} rw mitigations=off initrd=\initramfs-linux.img"
 EOF
 
 umount -R /mnt
